dc4417d70f5c7fe7294c8c0f66a0f171b4620523
zhr-zhang <zhr.zhang@outlook.com>
2023-01-20 21:24:17 +0800

    Debug Run()

--- The Three-body Problem Simulator/Run.cpp
+++ The Three-body Problem Simulator/Run.cpp
@@ -16,19 +16,25 @@ int Engine::run(int TimeLimit)
 			SpaceVector F; // 由star[j - 1]提供的力
 			SpaceVector resultantForce; // 合力
 
-			for (int j = i + 1; j % CelestialBody::quantity != i; j++)
+			for (int j = i + 1; j % CelestialBody::quantity != i; j++) // 循环下标防止溢出；
 			{
 				R.setSpaceVector(star[j % CelestialBody::quantity].getCoordinate() - star[i].getCoordinate());
 				F.setSpaceVector(R, GravitationalConstant * (star[i].getMass() * star[j % CelestialBody::quantity].getMass() / SQR(R.getModulus())));
-				resultantForce += F;
+				resultantForce += F; // 合力与当前受力合成
 
-				cout << "天体" << i << "到" << j << "距: " << R.getModulus() << endl;
-				cout << "天体" << j << "对" << i << "力: " << F.getModulus() << endl;
+				/*cout << "天体" << i << "到" << j << "距: " << R.getModulus() << endl;
+				cout << "天体" << j << "对" << i << "力: " << F.getModulus() << endl;*/
 			}
 			star[i].setForce(resultantForce);
 			star[i].setAcceleration(star[i].getForce() / star[i].getMass());
 			star[i].setVelocity(star[i].getVelocity() + star[i].getAcceleration() * DT);
 			star[i].setCoordinate(star[i].getCoordinate() + star[i].getVelocity() * DT);
+
+			//Debug
+			cout << star[i].getForce().getModulus() << endl;
+			cout << star[i].getAcceleration().getModulus() << endl;
+			cout << star[i].getVelocity().getModulus() << endl;
+			star[i].getCoordinate().print();
 		}
 
 		// 碰撞检测
@@ -43,10 +49,10 @@ int Engine::run(int TimeLimit)
 		}
 	}
 
-	cout << endl;
+	/*cout << endl;
 	for (int i = 0; i < CelestialBody::quantity; i++) {
 		star[i].getCoordinate().print();
-	}
+	}*/
 	return 0;
 }
 
