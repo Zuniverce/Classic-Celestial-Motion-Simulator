7ed7799ea73876fbcea36e47c64869e91f48afb9
张皓然 <zhr.zhang@outlook.com>
2023-03-29 23:18:11 +0800

    完善done

--- Classic Celestial Motion Simulator/Classic Celestial Motion Simulator.cpp
+++ Classic Celestial Motion Simulator/Classic Celestial Motion Simulator.cpp
@@ -7,8 +7,8 @@
 #include "MultidimensionalVector.h"
 int main()
 {
-	fin.open("IO/in.txt"); // 输入文件
-	fout.open("IO/out.txt"); //输出文件
+	fin.open("C:/in.txt"); // 输入文件
+	fout.open("C:/out.txt"); //输出文件
 	//cinbackup = cin.rdbuf(fin.rdbuf()); //用 rdbuf() 重新定向，返回旧缓冲区指针
 	//coutbackup = cout.rdbuf(fout.rdbuf()); //用 rdbuf() 重新定向，返回旧缓冲区指针
 	//MultidimensionalVector::setDimension();
@@ -16,8 +16,9 @@ int main()
 	Time::setTotalTime();
 	Engine instance;
 	instance.run();
-	system("pause");
 	fin.close();
 	fout.close();
+	cout << "输出文件在C:\\out.txt";
+	system("pause");
 	return 0;
 }
